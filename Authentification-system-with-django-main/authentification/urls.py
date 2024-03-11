83f3283a0fb5dafbedad5ec109e58c997a41e39f
aicha <khaouniaicha@gmail.com>
2024-03-11 15:05:18 +0100

    Premier commit

---
+++ Authentification-system-with-django-main/authentification/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from authentification import views
+from rest_framework.authtoken import views as drfviews
+from .views import geocode
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('api-token-auth/', drfviews.obtain_auth_token),
+    path('geocode', views.geocode, name='geocode'),   
+]
